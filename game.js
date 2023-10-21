17ddf29b0079dc9f79d7ad728ba51c51d41f89b7
Alfredo Lozano <alfredo.realizador@gmail.com>
2023-10-21 12:27:56 +0200

    Create 3 cels,broad,and neighbors functions and export it to game

---
+++ game.js
@@ -0,0 +1,7 @@
+import { randomBoard } from "./tools/create-board.js";
+import { countAliveNeighbors } from "./tools/create-board.js";
+import { celsConditions } from "./tools/cells-live.js";
+
+console.log(randomBoard);
+console.log(celsConditions);
+console.log(countAliveNeighbors());
